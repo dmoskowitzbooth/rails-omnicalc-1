58e08a381cae27d353119e4c007cf695f75b32d4
dmoskowitzbooth <165198611+dmoskowitzbooth@users.noreply.github.com>
2024-04-24 21:01:46 +0000

    1

--- app/controllers/master_controller.rb
+++ app/controllers/master_controller.rb
@@ -1,5 +1,16 @@
 class MasterController < ApplicationController
   def sq
-    render({ :template => pages_templates/sq})
+    render({ :template => "pages_templates/sq"})
+  end
+
+  def home
+    render({ :template => "pages_templates/sq"})
+  end
+
+  def sqr
+    @the_num=params.fetch("users_number")
+    @the_result=params.fetch("users_number").to_f*params.fetch("users_number").to_f
+    render({ :template => "pages_templates/sqr"})
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get("/", { :controller => "master", :action =>"home"})
   get("/square/new", { :controller => "master", :action =>"sq"})
-  get("/square/result", { :controller => "master", :action =>"sqr"})
+  get("/square/results", { :controller => "master", :action =>"sqr"})
 
 end
