9ec9b9620f8fbb7aa2234e474bab2fb12c566c4f
dmoskowitzbooth <165198611+dmoskowitzbooth@users.noreply.github.com>
2024-04-24 21:15:08 +0000

    fin

--- app/controllers/master_controller.rb
+++ app/controllers/master_controller.rb
@@ -13,4 +13,40 @@ class MasterController < ApplicationController
     render({ :template => "pages_templates/sqr"})
   end
 
+  def sqrt
+    render({ :template => "pages_templates/sqrt"})
+  end
+
+  def sqrtr
+    @the_num=params.fetch("users_number")
+    @the_result=params.fetch("users_number").to_f**0.5
+    render({ :template => "pages_templates/sqrtr"})
+  end
+
+  def ran
+    render({ :template => "pages_templates/ran"})
+  end
+
+  def ranr
+    @mini=params.fetch("users_min").to_f
+    @maxi=params.fetch("users_max").to_f
+    @results=rand(@mini..@maxi)
+    render({ :template => "pages_templates/ranr"})
+  end
+
+  def pay
+    render({ :template => "pages_templates/pay"})
+  end
+
+  def payr
+    @apr=params.fetch("users_apr").to_f
+    apr=@apr/100/12
+    @yrs=params.fetch("users_years").to_f
+    @prin=params.fetch("users_pv").to_f
+   
+    @results = (apr*@prin)/(1-(1+apr)**(@yrs*-12))
+    render({ :template => "pages_templates/payr"})
+  end
+  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   get("/", { :controller => "master", :action =>"home"})
   get("/square/new", { :controller => "master", :action =>"sq"})
   get("/square/results", { :controller => "master", :action =>"sqr"})
-
+  get("/square_root/new", { :controller => "master", :action =>"sqrt"})
+  get("/square_root/results", { :controller => "master", :action =>"sqrtr"})
+  get("/random/new", { :controller => "master", :action =>"ran"})
+  get("/random/results", { :controller => "master", :action =>"ranr"})
+  get("/payment/new", { :controller => "master", :action =>"pay"})
+  get("/payment/results", { :controller => "master", :action =>"payr"})
 end
